4d4dff0a23bfdf68585e2ba69d17b1caa9c058c1
一颗码农的心 <645556406@qq.com>
2025-10-04 22:11:53 +0800

    优化数据库使用

--- cmdb-backend/dao/dao_db.go
+++ cmdb-backend/dao/dao_db.go
@@ -55,29 +55,3 @@ func CloseDB() error {
 	}
 	return nil
 }
-
-//func NewDB() *gorm.DB {
-//	config := utils.LoadYamlConfigNew("config/dev.yaml")
-//	m := config["DB"].(map[string]interface{})
-//	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", m["username"], m["password"], m["hostname"], m["port"], m["database"])
-//	db, errDb := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-//	if errDb != nil {
-//		panic(errDb)
-//	}
-//	errAutoMigrate := db.AutoMigrate(&model.Server{}, &model.Role{}, &model.Departments{}, &model.User{})
-//	if errAutoMigrate != nil {
-//		log.Println(errAutoMigrate)
-//	}
-//	// 获取底层的sql.DB对象
-//	sqlDB, err := db.DB()
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	// 设置连接池参数
-//	sqlDB.SetMaxIdleConns(10)           // 最大空闲连接数
-//	sqlDB.SetMaxOpenConns(100)          // 最大打开连接数
-//	sqlDB.SetConnMaxLifetime(time.Hour) // 连接最大存活时间
-//
-//	return db
-//}
